5a3c7fe5100a2d912514b73d2fd555be5dbadb66
Joshua Greene <103796447+j02greene@users.noreply.github.com>
2023-04-26 23:54:30 -0400

    Update day5_passwordgenerator.py

--- day5_passwordgenerator.py
+++ day5_passwordgenerator.py
@@ -1,15 +1,15 @@
 import random
 import string
 print("Welcome to Password Generator\n=============================")
-chars = int(input("How many characters do you want in your password? "))
+letters = int(input("How many letters do you want in your password? "))
 special_chars = int(input("How many special characters do you want in your password? "))
 numbers = int(input("How many numbers do you want in your password? "))
 password = []
-random_chars_list = []
+random_letters_list = []
 
 
-for chars in range(chars):
-    random_chars_list.append(str(random.choice(string.ascii_lowercase + string.ascii_uppercase )))
+for chars in range(letters):
+    random_letters_list.append(str(random.choice(string.ascii_lowercase + string.ascii_uppercase )))
 
 special_chars_list = ['!', '@', '#', '$', '%', "^", '&', '*']
 numbers_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -20,7 +20,7 @@ for specialchars in range(special_chars):
 for nums in range(numbers):
     password.append(random.choice(numbers_list))
 
-combined = password + random_chars_list
+combined = password + random_letters_list
 
 random.shuffle(combined)
 
